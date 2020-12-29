ee66e7c5a516a91bf26cd714c549657415651b0d
gomez-addams <gomez-addams@users.noreply.github.com>
2020-12-29 00:31:29 -0700

    Add a script to aid creating an Intel macOS build, change the default build type

--- scripts/bootstrap-x86_64-macos.sh
+++ scripts/bootstrap-x86_64-macos.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# mac arm64 native build
+# mac x86_64 native build
 
 CMAKE_BIN=cmake
 #BUILD_TYPE=RelWithDebInfo
 BUILD_TYPE=Release
 
-rm -rf build-arm64-macos
-mkdir build-arm64-macos
+rm -rf build-x86_64-macos
+mkdir build-x86_64-macos
 
-cd build-arm64-macos
+cd build-x86_64-macos
 
 $CMAKE_BIN \
   -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-  -DEMBREE_ARM=On \
+  -DEMBREE_ARM=Off \
   -DEMBREE_IOS=On \
   -DCMAKE_INSTALL_PREFIX=$HOME/local/embree3 \
   -DEMBREE_ISPC_SUPPORT=Off \
@@ -21,9 +21,9 @@ $CMAKE_BIN \
   -DEMBREE_TUTORIALS=Off \
   -DEMBREE_RAY_PACKETS=Off \
   -DEMBREE_MAX_ISA=AVX2 \
-  -DEMBREE_NEON_AVX2_EMULATION=ON \
+  -DEMBREE_NEON_AVX2_EMULATION=Off \
   -DAS_MAC=ON \
-  -DCMAKE_OSX_ARCHITECTURES=arm64 \
+  -DCMAKE_OSX_ARCHITECTURES=x86_64 \
   ..
 
 cd ..
