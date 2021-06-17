c409fd0b1fbd9d4d076672f18a7dc21cadb9a7bc
gomez-addams <gomez-addams@users.noreply.github.com>
2021-06-17 16:21:24 -0600

    Another try at the macOS reported info

--- common/sys/sysinfo.cpp
+++ common/sys/sysinfo.cpp
@@ -47,7 +47,7 @@ namespace embree
     return "iOS Simulator (x64)";
 #elif defined(TARGET_OS_IPHONE) && defined(__aarch64__) && defined(__ARM_NEON)
     return "iOS (aarch64 / arm64)";
-#elif defined(__MACOSX__) && defined(__ARM_NEON)
+#elif defined(__MACOSX__) && (defined(__aarch64__) || defined(__ARM_NEON__) || defined(__ARM_NEON))
     return "Mac OS X (arm64)";
 #elif defined(__MACOSX__) && !defined(__X86_64__)
     return "Mac OS X (32bit)";
@@ -198,7 +198,7 @@ namespace embree
     case CPU::CORE2                   : return "Core2";
     case CPU::CORE1                   : return "Core";
     case CPU::ARM                     : return "Arm";
-#if defined(__MACOSX__) && defined(__ARM_NEON)
+#if defined(__MACOSX__) && (defined(__aarch64__) || defined(__ARM_NEON__) || defined(__ARM_NEON))
     case CPU::UNKNOWN                 : return "Apple Silicon";
 #else
     case CPU::UNKNOWN                 : return "Unknown CPU";
